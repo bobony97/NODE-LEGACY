e80ae0787f65df3a6a101ed54a7801f3057117dd
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-06 20:29:28 -0300

    se creo el archivo package.json con su configuracion, se instalo yargs para manejar la informacion de la consola

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -1,14 +1,21 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
+//FORMA TRADICIONAL DE TRAER INFORMACION DE LA CONSOLA
+//Para que esto funcione se debe enviar el comando de ejecucion del programa junto a un tercer argumento para que se lo pueda desestructurar EJ: node app.js --base=5
 //Esto extrae la informacion que llega de la consola
-const [, , arg3] = process.argv;
+// const [, , arg3] = process.argv;
 //Esto extrae el valor que obtenemos de la consola
-const [, base = 5] = arg3.split('=');
+// const [, base = 5] = arg3.split('=');
+
+console.log(process.argv);
+console.log(argv);
+console.log('base yargs ' + argv.base);
 
 // const base = 3;
 
-crearArchivo( base )
-    .then((nombreArchivo) => console.log(nombreArchivo))
-    .catch((err) => console.log(err));
+// crearArchivo( base )
+//     .then((nombreArchivo) => console.log(nombreArchivo))
+//     .catch((err) => console.log(err));
