d10796d287bb14c0b681724d01dbf0d166b02b5d
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-06 12:51:44 -0300

    implementacion para recibir informacion a traves de la consola

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -2,7 +2,12 @@ const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-const base = 3;
+//Esto extrae la informacion que llega de la consola
+const [, , arg3] = process.argv;
+//Esto extrae el valor que obtenemos de la consola
+const [, base = 5] = arg3.split('=');
+
+// const base = 3;
 
 crearArchivo( base )
     .then((nombreArchivo) => console.log(nombreArchivo))
