ea8a0a07c6a32335ae7bb88d29a2020511726300
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-08 00:40:41 -0300

    se instalo inquirer y se hizo un nuevo menu interactivo

--- 03-tareas-hacer/app.js
+++ 03-tareas-hacer/app.js
@@ -1,6 +1,7 @@
 require('colors');
 
-const { showMenu, pause } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
+const { pause } = require('./helpers/mensajes');
 
 const main = async() => {
     console.clear();
@@ -8,7 +9,7 @@ const main = async() => {
     let opt = '';
 
     do {
-       opt = await showMenu();
+       opt = await inquirerMenu();
        console.log(opt)
        if(opt !== '0') await pause();
     } while(opt !== '0');
