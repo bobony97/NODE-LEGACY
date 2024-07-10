1f35aaa82b590b0f5b4102f4348a3f91ac790c44
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-09 23:27:30 -0300

    ejemplo completo de la creacion de un servidor web con node puro

--- 03-webserver/app.js
+++ 03-webserver/app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 http.createServer( (request, response) => {
-    response.write('Hola Mundo');
+
+    //Esto se utiliza para establecer un encabezado HTTP en la respuesta antes de que se envíe al cliente. 
+    //Este método es parte del objeto response, que representa la respuesta HTTP que se enviará al cliente. 
+    //A diferencia de writeHead, que establece todos los encabezados de una vez, setHeader se utiliza para configurar encabezados individuales de manera incremental.
+    response.setHeader('Content-Disposition', 'attachment: filename=Lista.csv');
+
+    //Esto  permite establecer el código de estado HTTP y los encabezados de respuesta antes de enviar el cuerpo de la respuesta. 
+    //Es útil para especificar el tipo de contenido, codificaciones, cookies, y otros metadatos de la respuesta HTTP.
+    response.writeHead(200, {'Content-Type': 'application/csv'});
+
+    // Escribe datos en el cuerpo de la respuesta y se puede llamar múltiples veces antes de finalizar la respuesta.
+    response.write('id, nombre\n');
+    response.write('1, pedro\n');
+    response.write('2, pe\n');
+    response.write('3, juan\n');
+
+    // Finaliza la respuesta, opcionalmente escribiendo datos adicionales. Debe llamarse para que el servidor sepa que se han terminado de enviar datos y pueda cerrar la conexión.
     response.end();
 })
 .listen( 3000 );
