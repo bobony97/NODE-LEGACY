663ae623f0cdbe278b5a5755ec4337a25bc9f9f0
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-11 01:07:36 -0300

    se instalo el motor de plantillas hbs, se hicieron configuraciones iniciales y se renderizo la vista del home

--- 03-webserver/app.js
+++ 03-webserver/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Configura el motor de plantillas que se utilizará para renderizar las vistas. En este caso, está configurando Handlebars (hbs) como el motor de plantillas predeterminado.
+app.set('view engine', 'hbs');
 
 //middleware para contenido statico
 app.use(express.static('public'));
-
 
 // app.get('/', (req, res) =>{
 //     res.send('Home');
@@ -13,6 +16,10 @@ app.use(express.static('public'));
 //     res.send('Hola Mundo');
 // });
 
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
 })
@@ -21,9 +28,10 @@ app.get('/elements', (req, res) => {
 })
 
 //Redirecciona al usuario a otro archivo cuando se ingresa una ruta que no este dentro de la aplicacion
-app.get('*', (req, res) =>{
-    res.sendFile(__dirname + '/public/404.html');
-});
+// app.get('*', (req, res) =>{
+//     res.sendFile(__dirname + '/public/404.html');
+// });
+
 
 app.listen(8080);
 
