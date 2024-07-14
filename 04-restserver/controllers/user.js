0dfe8f448944527ab7e17a9860e5870c266c2089
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-13 23:57:34 -0300

    obteniendo informacion de las peticiones a traves de las rutas

--- 04-restserver/controllers/user.js
+++ 04-restserver/controllers/user.js
@@ -1,19 +1,25 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const getUsers = (req, res = response) => {
+const getUsers = (req = request, res = response) => {
+    //Obtenemos toda la informacion enviada por la ruta 
+    const params = req.query;
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        params
     });
 };
 
-const putUsers = (req, res) => {
+const putUsers = (req, res = response) => {
+    //Obtenemos la informacion que el usuario mande en la ruta en la peticion put
+    const id = req.params.id
     res.json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     });
 };
 
 const postUsers = (req, res) => {
-    //Obtenemos la informacion que el usuario mande en la peticion post
+    //Obtenemos la informacion que el usuario mande en el body en la peticion post
     const body = req.body;
     res.json({
         msg: 'post API - controlador',
