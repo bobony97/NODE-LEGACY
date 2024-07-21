a1194a06fde88f4a8b346fc6889fdab3b6130d9a
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-20 22:45:36 -0300

    implementacion de la generacion del jwt y validaciones para el login

--- 04-restserver/controllers/auth.js
+++ 04-restserver/controllers/auth.js
@@ -1,11 +1,49 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/usuario');
+const { generateJWT } = require('../helpers/generateJWT');
 
-const login = (req, res = response) => {
+const login = async(req, res = response) => {
 
+    try {
+        const { email, password } = req.body;
+        
+        //Verifica si el usuario existe
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+        
+        //Verifica si el usuario esta activo
+        if(!user.state) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
 
-    res.json({
-        msg: 'Holi'
-    });
+        //Verifica la contraseña
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if(!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        const token = await generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Algo salio mal, contacte con el administrador'
+        });
+    }
 };
 
 
