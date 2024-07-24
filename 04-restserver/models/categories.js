f7cc4647d6eeb589e7210b810ecfbd783171ff0f
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-23 23:57:30 -0300

    creacion del modelo para la categoria

---
+++ 04-restserver/models/categories.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const categoriesSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    user: {
+        //Indica que el tipo va a ser otro objeto en mongo
+        type: Schema.Types.ObjectId,
+        //Hace referencia al otro objeto, especificado en el model ej: "module.exports = model('Category', categorySchema)", en este caso seria "Category"
+        ref: 'User',
+        required: true
+    }
+});
+
+
+module.exports = model('Category', categoriesSchema);
