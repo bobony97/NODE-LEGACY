3d9e96cf124660c05675a5e78cf1cbcd29862172
Fernando Agustin Perez <aguss.97@outlook.com>
2024-07-29 23:24:37 -0300

    se implementa la busqueda por varios argumentos

--- 04-restserver/controllers/search.js
+++ 04-restserver/controllers/search.js
@@ -19,10 +19,23 @@ const searchUsers = async(terminus = '', res = response) => {
 
     if(isMongoId) {
         const user = await User.findById(terminus);
-        res.status(200).json({
+        return res.status(200).json({
             results: (user) ? [ user ] : []
         });
     };
+
+    //Se utiliza esta expresion regular para indicar que la busqueda va a ser insensible a las mayusculas y minusculas
+    const regEx = new RegExp(terminus, 'i');
+
+    //Indicamos que la expresion regular tiene que hacer match con el nombre o con el correo
+    const users = await User.find({ 
+        $or: [ {name: regEx}, { email: regEx }],
+        $and: [{ state: true }]
+    });
+
+    res.status(200).json({
+        results: users
+    });
 };
 
 const search = async(req, res = response) => {
