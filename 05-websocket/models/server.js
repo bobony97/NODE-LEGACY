c8776eeca46c889932806ac28e4765734daf6ca3
Fernando Agustin Perez <aguss.97@outlook.com>
2024-08-06 13:04:33 -0300

    se implemento un escuchador para saber cuando el usuario se conecta y se desconecta del socket

--- 05-websocket/models/server.js
+++ 05-websocket/models/server.js
@@ -6,11 +6,20 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        //Esto crea el servidor para los sockets
-        this.server = require('http').createServer(this.app);
-        this.io = require('socket.io')(this.server);
         this.middlewares();
         this.routes();
+
+        //require('http'): Importa el módulo HTTP nativo de Node.js, que se utiliza para crear servidores web.
+        //Esto crea el servidor para los sockets
+        this.server = require('http').createServer(this.app);
+
+        //(this.server): Inicializa Socket.IO con el servidor HTTP creado en la línea anterior 
+        //Esto permite que Socket.IO escuche y gestione las conexiones en tiempo real en el mismo servidor HTTP que maneja las solicitudes de la aplicación Express.
+        this.io = require('socket.io')(this.server);
+
+        //Configuracion de sockets
+        this.sockets();
+
     };
 
     middlewares() {
@@ -21,6 +30,20 @@ class Server {
 
     routes() {
 
+    };
+
+    sockets() {
+        //Esto configura un manejador de eventos para el evento connection. Este evento se emite cada vez que un nuevo cliente se conecta al servidor mediante Socket.IO.
+        //La función de callback que recibe como argumento el objeto socket representa la conexión individual con el cliente que se acaba de conectar.
+        this.io.on('connection', socket => {
+            console.log('Cliente Conectado', socket.id); //Imprime en la consola un mensaje indicando que un cliente se ha conectado, junto con el id único del socket correspondiente 
+                                                        //a ese cliente.
+                                                        
+            //Configura un manejador de eventos para el evento disconnect. Este evento se emite cuando el cliente se desconecta del servidor.
+            socket.on('disconnect', () => {
+                console.log('Cliente Desconectado', socket.id);
+            });
+        });
     };
 
     listen() {
