2f35618c951c310c61cdd8104b4d7725f858bd95
Fernando Agustin Perez <aguss.97@outlook.com>
2024-08-06 13:33:25 -0300

    se implemento listener para enviar mensaje de conexion y desconexion del cliente

--- 05-websocket/public/js/socket-client.js
+++ 05-websocket/public/js/socket-client.js
@@ -1,3 +1,19 @@
 //ACA SE ENCUENTRA LA INFORMACION DE LOS SOCKETS CON EL CLIENTE
 
+//Referencias del html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
